0515e0e41934f05258a71a0a565bcd60c80a8f31
Fazil <faziltirtana@tutanota.com>
2023-11-08 08:42:39 +0700

    Deploy

---
+++ _next/static/chunks/app/the-team/page-b7f7652eb44917ff.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[733,711],{4999:function(e,n,s){Promise.resolve().then(s.t.bind(s,3222,23)),Promise.resolve().then(s.t.bind(s,6685,23))}},function(e){e.O(0,[685,222,971,596,744],function(){return e(e.s=4999)}),_N_E=e.O()}]);
