c9dccc562e2889fc36079faa1ea77113816c2945
Fazil <faziltirtana@tutanota.com>
2023-11-08 10:56:02 +0700

    Deploy

---
+++ _next/static/chunks/app/cstudy/page-51bfdaa20d45be6c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[753],{48186:function(n,e,u){Promise.resolve().then(u.t.bind(u,46685,23))}},function(n){n.O(0,[685,971,596,744],function(){return n(n.s=48186)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/the-team/page-3825b1aa4eb0f1d8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[733,711],{14999:function(e,n,s){Promise.resolve().then(s.t.bind(s,63222,23)),Promise.resolve().then(s.t.bind(s,46685,23))}},function(e){e.O(0,[685,222,971,596,744],function(){return e(e.s=14999)}),_N_E=e.O()}]);
