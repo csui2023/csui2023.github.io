354f63d2a34249a1c14d117a8f439357283c02ad
secona <77039267+secona@users.noreply.github.com>
2023-11-11 06:53:16 +0700

    deploy

---
+++ _next/static/chunks/app/the-fam/[id]/page-1cb00191e642a43b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[711],{55299:function(e,n,s){Promise.resolve().then(s.t.bind(s,46685,23)),Promise.resolve().then(s.t.bind(s,63222,23))}},function(e){e.O(0,[685,222,971,596,744],function(){return e(e.s=55299)}),_N_E=e.O()}]);
