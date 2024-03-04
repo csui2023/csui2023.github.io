37edeb394a3297f1585fe2d89d02e0b94a909ed4
secona <77039267+secona@users.noreply.github.com>
2024-03-04 20:17:52 +0700

    deploy

---
+++ _next/static/chunks/app/cstudy/page-03bd97825d229ea4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[753],{38550:function(n,e,u){Promise.resolve().then(u.t.bind(u,46685,23))}},function(n){n.O(0,[685,971,596,744],function(){return n(n.s=38550)}),_N_E=n.O()}]);
