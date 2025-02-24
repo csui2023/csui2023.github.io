1007c86d1fffae0d1802a8dd72266be8f1cae6cd
secona <77039267+secona@users.noreply.github.com>
2025-02-24 10:31:55 +0700

    init

---
+++ _next/static/UGdVf_YOMEqVBBuBdu1fS/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fthe-fam\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/chunks/app/the-fam/[id]/page-ee35f199edabbeec.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[711],{48194:function(e,n,s){Promise.resolve().then(s.t.bind(s,63222,23)),Promise.resolve().then(s.t.bind(s,46685,23))}},function(e){e.O(0,[685,222,971,596,744],function(){return e(e.s=48194)}),_N_E=e.O()}]);
